cea3b99ec47aab281f1c0b8a37c03da157f4ac11
MikoMikarro <lopezcuestam@gmail.com>
2025-08-03 00:16:46 +0200

    created header

--- src/app/landing/landing-header/landing-header.component.ts
+++ src/app/landing/landing-header/landing-header.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { NbThemeService } from '@nebular/theme';
+
+
 @Component({
   selector: 'ls-landing-header',
   templateUrl: './landing-header.component.html',
   styleUrls: ['./landing-header.component.scss']
 })
-export class LandingHeaderComponent{
+export class LandingHeaderComponent implements OnInit{
 
+  currentTheme = 'default';
+
+  constructor(private themeService: NbThemeService){
+    
+  }
+
+  toggleTheme() {
+    if (this.currentTheme == 'default')
+      this.currentTheme = 'cosmic';
+    else
+      this.currentTheme = 'default';
+
+    this.themeService.changeTheme(this.currentTheme);
+
+  }
+
+  ngOnInit() {
+    this.currentTheme = this.themeService.currentTheme;
+  }
+  
 }
